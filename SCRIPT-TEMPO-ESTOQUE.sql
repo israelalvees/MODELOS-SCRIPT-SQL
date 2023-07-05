0cd0ce28673c319c9de327faa998207c37c1b903
Israel Alves <128307729+israelalvees@users.noreply.github.com>
2023-07-05 17:59:27 -0300

    Update SCRIPT-TEMPO-ESTOQUE.sql

--- SCRIPT-TEMPO-ESTOQUE.sql
+++ SCRIPT-TEMPO-ESTOQUE.sql
@@ -1,13 +1,22 @@
 
--- Seleciona o código do produto, descrição, tempo de estoque, fornecedor e seção do estoque
-SELECT CODIGO_PRODUTO, DESCRICAO, TEMPO_ESTOQUE, FORNECEDOR, SECAO
+SELECT CODIGO_PRODUTO, DESCRICAO, ESTOQUE_ATUAL, TEMPO_ESTOQUE, FORNECEDOR, SECAO, QUANTIDADE_VENDIDA
 FROM (
-    -- Subconsulta para realizar o cálculo do tempo de estoque para cada produto
+    -- Subconsulta para realizar o cálculo do tempo de estoque e quantidade vendida para cada produto
     SELECT E.PRO_CODI AS CODIGO_PRODUTO,
            P.PRO_DESC AS DESCRICAO,
-           ROUND(E.EST_QUAN / (F.SUM_QUANTIDADE / 6), 0) AS TEMPO_ESTOQUE,
+           E.EST_QUAN AS ESTOQUE_ATUAL,
+           CASE 
+               WHEN F.SUM_QUANTIDADE = 0 THEN NULL 
+               ELSE ROUND(E.EST_QUAN / (F.SUM_QUANTIDADE / 6), 0) 
+           END AS TEMPO_ESTOQUE,
            P.FOR_CODI AS FORNECEDOR,
            P.SEC_CODI AS SECAO,
+           (
+               SELECT SUM(QUANTIDADE) 
+               FROM VW_FATURAMENTO_DETALHADO 
+               WHERE CODIGO_PRODUTO = E.PRO_CODI 
+                     AND DATA BETWEEN '01-01-2023' AND '30-06-2023'
+           ) AS QUANTIDADE_VENDIDA,
            ROW_NUMBER() OVER (PARTITION BY E.PRO_CODI ORDER BY E.PRO_CODI) AS RowNum
     FROM ESTOQUE E
     INNER JOIN PRODUTO P ON E.PRO_CODI = P.PRO_CODI
@@ -15,11 +24,11 @@ FROM (
         -- Subconsulta para calcular a quantidade média vendida por semana para cada produto no período desejado
         SELECT CODIGO_PRODUTO, SUM(QUANTIDADE) AS SUM_QUANTIDADE
         FROM VW_FATURAMENTO_DETALHADO
-        WHERE DATA BETWEEN '2023-01-01' AND '2023-06-30'
+        WHERE DATA BETWEEN '01-01-2023' AND '30-06-2023'
         GROUP BY CODIGO_PRODUTO
     ) F ON E.PRO_CODI = F.CODIGO_PRODUTO
-    --WHERE P.FOR_CODI = '00940' --filtro por fornecedor específico
+    WHERE P.FOR_CODI = '00940' --filtro por fornecedor específico
 ) T
 WHERE RowNum = 1
-ORDER BY DESCRICAO, TEMPO_ESTOQUE
+ORDER BY DESCRICAO, TEMPO_ESTOQUE;
 
