11254939ca90f9db2fc515b8e78727563a95ea11
Israel Alves <128307729+israelalvees@users.noreply.github.com>
2023-07-06 12:45:10 -0300

    Update SCRIPT-TEMPO-ESTOQUE.sql

--- SCRIPT-TEMPO-ESTOQUE.sql
+++ SCRIPT-TEMPO-ESTOQUE.sql
@@ -1,7 +1,7 @@
 
 SELECT CODIGO_PRODUTO, DESCRICAO, ESTOQUE_ATUAL, TEMPO_ESTOQUE, FORNECEDOR, SECAO, QUANTIDADE_VENDIDA
 FROM (
-    -- Subconsulta para realizar o cálculo do tempo de estoque e quantidade vendida para cada produto
+    -- Subconsulta para realizar o cálculo do tempo de estoque 
     SELECT E.PRO_CODI AS CODIGO_PRODUTO,
            P.PRO_DESC AS DESCRICAO,
            E.EST_QUAN AS ESTOQUE_ATUAL,
@@ -21,7 +21,7 @@ FROM (
     FROM ESTOQUE E
     INNER JOIN PRODUTO P ON E.PRO_CODI = P.PRO_CODI
     INNER JOIN (
-        -- Subconsulta para calcular a quantidade média vendida por semana para cada produto no período desejado
+        -- Subconsulta para calcular a quantidade vendida para cada produto no período desejado
         SELECT CODIGO_PRODUTO, SUM(QUANTIDADE) AS SUM_QUANTIDADE
         FROM VW_FATURAMENTO_DETALHADO
         WHERE DATA BETWEEN '01-01-2023' AND '30-06-2023'
