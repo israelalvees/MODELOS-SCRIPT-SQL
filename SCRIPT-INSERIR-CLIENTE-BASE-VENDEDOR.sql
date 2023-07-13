6f176b919ef66a093fd215db3344b82151a49f36
Israel Alves <128307729+israelalvees@users.noreply.github.com>
2023-07-13 11:36:28 -0300

    Update SCRIPT-INSERIR-CLIENTE-BASE-VENDEDOR.sql

--- SCRIPT-INSERIR-CLIENTE-BASE-VENDEDOR.sql
+++ SCRIPT-INSERIR-CLIENTE-BASE-VENDEDOR.sql
@@ -22,3 +22,10 @@ INSERT INTO TBCLIENTE_VENDEDOR (CLI_CODI,VEN_CODI,TPV_CODI,VEN_CODI_ANT,CXV_STSU
 SELECT CLI_CODI,'CODIGO DO VENDEDOR',NULL,NULL,'I' FROM CLIENTE 
 WHERE  UND_CDNAC ='CE'
 AND    MUN_CODI ='03709'
+
+--------------------------SELECT CONFIRMAÇÃO------------------------------------------
+
+--somente para contar a quantidade de clientes, validar que foram inseridos todos
+SELECT COUNT(CLI_CODI)
+FROM TBCLIENTE_VENDEDOR
+WHERE VEN_CODI = 'codigovendedor'
