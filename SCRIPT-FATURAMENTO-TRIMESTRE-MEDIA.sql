33b91bfc369591e5f7ae8898bc9ebe271b008771
Israel Alves <128307729+israelalvees@users.noreply.github.com>
2023-09-15 16:17:35 -0300

    Update SCRIPT-FATURAMENTO-TRIMESTRE-MEDIA.sql

--- SCRIPT-FATURAMENTO-TRIMESTRE-MEDIA.sql
+++ SCRIPT-FATURAMENTO-TRIMESTRE-MEDIA.sql
@@ -1,62 +1,53 @@
 SELECT 
     CODIGO_PRODUTO,
     PRODUTO,
-    SUM(CASE WHEN DATA BETWEEN '01/09/2023' AND '30/09/2023' THEN QUANTIDADE END) AS QUANTIDADE,
-    CAST(
-        (
-            SELECT SUM(QUANTIDADE) / 3
-            FROM VW_FATURAMENTO_DETALHADO AS SubQuery
-            WHERE DATA BETWEEN '01/06/2023' AND '31/08/2023'
-            AND NATUREZA = 'VEN'
-            AND SubQuery.CODIGO_PRODUTO = VW_FATURAMENTO_DETALHADO.CODIGO_PRODUTO
-        ) AS INT
-    ) AS MEDIA_ULTIMO_TRIMESTRE,  
-    FOR_CODI, 
-    FORNECEDOR 
-FROM VW_FATURAMENTO_DETALHADO
-WHERE LOCALIZACAO = '001'
-AND FOR_CODI = 940
-AND NATUREZA = 'VEN'
-GROUP BY CODIGO_PRODUTO, PRODUTO, FOR_CODI, FORNECEDOR
-
-
-
-
-SELECT 
-    CODIGO_PRODUTO,
-    PRODUTO,
-    SUM(CASE WHEN DATA BETWEEN '01/09/2023' AND '30/09/2023' THEN QUANTIDADE END) AS QUANTIDADE,
-    CAST(
-        (
-            SELECT SUM(QUANTIDADE) / 3
-            FROM VW_FATURAMENTO_DETALHADO AS SubQuery
-            WHERE DATA BETWEEN '01/06/2023' AND '31/08/2023'
-            AND NATUREZA = 'VEN'
-            AND SubQuery.CODIGO_PRODUTO = VW_FATURAMENTO_DETALHADO.CODIGO_PRODUTO
-        ) AS INT
-    ) AS MEDIA_ULTIMO_TRIMESTRE,  
-    ((SUM(CASE WHEN DATA BETWEEN '01/09/2023' AND '30/09/2023' THEN QUANTIDADE END) - 
-      CAST(
+    COALESCE(SUM(CASE WHEN CONVERT(DATE, DATA, 103) BETWEEN '2023-09-01' AND '2023-09-30' THEN QUANTIDADE END), 0) AS QUANTIDADE,
+    COALESCE(
+        CAST(
             (
                 SELECT SUM(QUANTIDADE) / 3
                 FROM VW_FATURAMENTO_DETALHADO AS SubQuery
-                WHERE DATA BETWEEN '01/06/2023' AND '31/08/2023'
+                WHERE CONVERT(DATE, DATA, 103) BETWEEN '2023-06-01' AND '2023-08-31'
                 AND NATUREZA = 'VEN'
                 AND SubQuery.CODIGO_PRODUTO = VW_FATURAMENTO_DETALHADO.CODIGO_PRODUTO
             ) AS INT
-        )) / NULLIF(CAST(
+        ), 0
+    ) AS MEDIA_ULTIMO_TRIMESTRE,  
+    COALESCE(
+        CAST(
             (
-                SELECT SUM(QUANTIDADE) / 3
-                FROM VW_FATURAMENTO_DETALHADO AS SubQuery
-                WHERE DATA BETWEEN '01/06/2023' AND '31/08/2023'
-                AND NATUREZA = 'VEN'
-                AND SubQuery.CODIGO_PRODUTO = VW_FATURAMENTO_DETALHADO.CODIGO_PRODUTO
+                ((SUM(CASE WHEN CONVERT(DATE, DATA, 103) BETWEEN '2023-09-01' AND '2023-09-30' THEN QUANTIDADE END) - 
+                COALESCE(
+                    CAST(
+                        (
+                            SELECT SUM(QUANTIDADE) / 3
+                            FROM VW_FATURAMENTO_DETALHADO AS SubQuery
+                            WHERE CONVERT(DATE, DATA, 103) BETWEEN '2023-06-01' AND '2023-08-31'
+                            AND NATUREZA = 'VEN'
+                            AND SubQuery.CODIGO_PRODUTO = VW_FATURAMENTO_DETALHADO.CODIGO_PRODUTO
+                        ) AS INT
+                    ), 0
+                )) / NULLIF(
+                    COALESCE(
+                        CAST(
+                            (
+                                SELECT SUM(QUANTIDADE) / 3
+                                FROM VW_FATURAMENTO_DETALHADO AS SubQuery
+                                WHERE CONVERT(DATE, DATA, 103) BETWEEN '2023-06-01' AND '2023-08-31'
+                                AND NATUREZA = 'VEN'
+                                AND SubQuery.CODIGO_PRODUTO = VW_FATURAMENTO_DETALHADO.CODIGO_PRODUTO
+                            ) AS INT
+                        ), 0
+                    ), 0
+                )) * 100
             ) AS INT
-        ), 0)) * 100 AS CRESCIMENTO_RELATIVO,
+        ), 0
+    ) AS CRESCIMENTO_RELATIVO,
     FOR_CODI, 
     FORNECEDOR
 FROM VW_FATURAMENTO_DETALHADO
 WHERE LOCALIZACAO = '001'
 AND FOR_CODI = 940
-AND NATUREZA = 'VEN'	
+AND NATUREZA = 'VEN'
 GROUP BY CODIGO_PRODUTO, PRODUTO, FOR_CODI, FORNECEDOR
+ORDER BY PRODUTO
